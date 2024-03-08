8de0a351127c10ed938c6b4f8fa9d6fde8e133ae
Gikubu <davidndungu8203@gmail.com>
2024-03-08 23:20:51 +0300

    this creates a class to read animal names from a file.

--- lib/week4project.dart
+++ lib/week4project.dart
@@ -1,3 +1,8 @@
-int calculate() {
-  return 6 * 7;
+// Class to read animal names from a file
+class AnimalFileReader {
+  List<String> readDataFromFile(String filePath) {
+    // Assume reading data from file logic here
+    // For simplicity, return a list of animal names
+    return ['Dog', 'Cat'];
+  }
 }
