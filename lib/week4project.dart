710e43e7e493352e0eaf2774e53e994b311a65af
Gikubu <davidndungu8203@gmail.com>
2024-03-08 23:24:13 +0300

    this defined a superclass implementing the animal interface.

--- lib/week4project.dart
+++ lib/week4project.dart
@@ -3,6 +3,15 @@ abstract class Animal {
   void makeSound(); // Abstract method that must be implemented by subclasses
 }
 
+// Define a superclass implementing the Animal interface
+class AnimalBase implements Animal {
+  // Override the makeSound method to provide a generic implementation
+  @override
+  void makeSound() {
+    print('Generic animal sound');
+  }
+}
+
 // Class to read animal names from a file
 class AnimalFileReader {
   List<String> readDataFromFile(String filePath) {
